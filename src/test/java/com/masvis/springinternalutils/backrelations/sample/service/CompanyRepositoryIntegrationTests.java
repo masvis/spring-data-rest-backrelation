dd6db422e7191936d6b8ca03f3a48c1a5331ef9f
Ruj89 <ruggero.russo89@gmail.com>
2018-02-07 09:58:18 +0100

    Implementation of the last stage of tests

--- src/test/java/com/masvis/springinternalutils/backrelations/sample/service/CompanyRepositoryIntegrationTests.java
+++ src/test/java/com/masvis/springinternalutils/backrelations/sample/service/CompanyRepositoryIntegrationTests.java
@@ -17,15 +17,14 @@ package com.masvis.springinternalutils.backrelations.sample.service;
 
 import com.masvis.springinternalutils.backrelations.sample.domain.City;
 import com.masvis.springinternalutils.backrelations.sample.domain.Company;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
@@ -35,6 +34,7 @@ import static org.springframework.http.MediaType.valueOf;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class CompanyRepositoryIntegrationTests {
     @Autowired
     CityRepository cityRepository;
@@ -44,43 +44,162 @@ public class CompanyRepositoryIntegrationTests {
     private TestRestTemplate restTemplate;
 
     @Test
-    public void exampleTest() {
+    public void test1FillRest() {
+        HttpEntity<String> tHttpEntity;
+        ResponseEntity<String> body;
+
         City bari = new City();
         bari.setName("Bari");
         City conversano = new City();
         conversano.setName("Conversano");
+        City lecce = new City();
+        lecce.setName("Lecce");
+        City brindisi = new City();
+        brindisi.setName("Brindisi");
+        City rutigliano = new City();
+        rutigliano.setName("Rutigliano");
+        City cassano = new City();
+        cassano.setName("Cassano delle Murge");
+        City gioia = new City();
+        gioia.setName("Gioia del Colle");
 
         Company politecnico = new Company();
         politecnico.setName("Politecnico");
         Company masvis = new Company();
         masvis.setName("Masvis");
+        Company university = new Company();
+        masvis.setName("Università degli studi di Bari");
 
         bari = this.restTemplate.postForObject("/cities", bari, City.class);
         conversano = this.restTemplate.postForObject("/cities", conversano, City.class);
+        lecce = this.restTemplate.postForObject("/cities", lecce, City.class);
+        brindisi = this.restTemplate.postForObject("/cities", brindisi, City.class);
+        rutigliano = this.restTemplate.postForObject("/cities", rutigliano, City.class);
+        cassano = this.restTemplate.postForObject("/cities", cassano, City.class);
+        gioia = this.restTemplate.postForObject("/cities", gioia, City.class);
 
         politecnico = this.restTemplate.postForObject("/companies", politecnico, Company.class);
         masvis = this.restTemplate.postForObject("/companies", masvis, Company.class);
+        university = this.restTemplate.postForObject("/companies", university, Company.class);
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(valueOf("text/uri-list"));
-        HttpEntity<String> tHttpEntity = new HttpEntity<>("/cities/"+bari.getId(), headers);
-        ResponseEntity<String> body = this.restTemplate.exchange("/companies/"+politecnico.getId()+"/cities", HttpMethod.PUT, tHttpEntity, String.class);
+
+        tHttpEntity = new HttpEntity<>("/cities/" + bari.getId(), headers);
+        body = this.restTemplate.exchange("/companies/" + politecnico.getId() + "/cities", HttpMethod.POST, tHttpEntity, String.class);
+        assertThat(body.getStatusCode() == HttpStatus.CREATED);
+
+        tHttpEntity = new HttpEntity<>("/cities/" + conversano.getId(), headers);
+        body = this.restTemplate.exchange("/companies/" + politecnico.getId() + "/cities", HttpMethod.POST, tHttpEntity, String.class);
+        assertThat(body.getStatusCode() == HttpStatus.CREATED);
+        tHttpEntity = new HttpEntity<>("/cities/" + conversano.getId(), headers);
+        body = this.restTemplate.exchange("/companies/" + masvis.getId() + "/cities", HttpMethod.POST, tHttpEntity, String.class);
+        assertThat(body.getStatusCode() == HttpStatus.CREATED);
+
+        tHttpEntity = new HttpEntity<>("/cities/" + brindisi.getId(), headers);
+        body = this.restTemplate.exchange("/companies/" + politecnico.getId() + "/cities", HttpMethod.POST, tHttpEntity, String.class);
+        assertThat(body.getStatusCode() == HttpStatus.CREATED);
+        tHttpEntity = new HttpEntity<>("/cities/" + lecce.getId(), headers);
+        body = this.restTemplate.exchange("/companies/" + politecnico.getId() + "/cities", HttpMethod.POST, tHttpEntity, String.class);
+        assertThat(body.getStatusCode() == HttpStatus.CREATED);
+        tHttpEntity = new HttpEntity<>("/cities/" + cassano.getId(), headers);
+        body = this.restTemplate.exchange("/companies/" + masvis.getId() + "/cities", HttpMethod.POST, tHttpEntity, String.class);
+        assertThat(body.getStatusCode() == HttpStatus.CREATED);
+        tHttpEntity = new HttpEntity<>("/cities/" + gioia.getId(), headers);
+        body = this.restTemplate.exchange("/companies/" + masvis.getId() + "/cities", HttpMethod.POST, tHttpEntity, String.class);
+        assertThat(body.getStatusCode() == HttpStatus.CREATED);
+
+        tHttpEntity = new HttpEntity<>("/cities/" + rutigliano.getId(), headers);
+        body = this.restTemplate.exchange("/companies/" + university.getId() + "/cities", HttpMethod.PUT, tHttpEntity, String.class);
+        assertThat(body.getStatusCode() == HttpStatus.CREATED);
+        tHttpEntity = new HttpEntity<>("/cities/" + brindisi.getId(), headers);
+        body = this.restTemplate.exchange("/companies/" + university.getId() + "/cities", HttpMethod.PUT, tHttpEntity, String.class);
+        assertThat(body.getStatusCode() == HttpStatus.CREATED);
+
+        tHttpEntity = new HttpEntity<>(null, headers);
+        body = this.restTemplate.exchange("/companies/" + masvis.getId() + "/cities/" + masvis.getId(), HttpMethod.DELETE, tHttpEntity, String.class);
+        assertThat(body.getStatusCode() == HttpStatus.ACCEPTED);
     }
 
     @Test
-    public void executesQueryMethodsCorrectly() {
+    public void test2BariCompaniesTest() {
         List<City> bariArray = this.cityRepository.findByName("Bari");
-        List<City> conversanoArray = this.cityRepository.findByName("Conversano");
         assertThat(bariArray).hasSize(1);
-        assertThat(conversanoArray).hasSize(1);
 
         City bari = bariArray.get(0);
-        City conversano = conversanoArray.get(0);
 
         List<Company> companiesInBari = this.companyRepository.findByCitiesContaining(bari);
+        assertThat(companiesInBari).hasSize(1);
+    }
+
+    @Test
+    public void test3ConversanoCompaniesTest() {
+        List<City> conversanoArray = this.cityRepository.findByName("Conversano");
+        assertThat(conversanoArray).hasSize(1);
+
+        City conversano = conversanoArray.get(0);
+
         List<Company> companiesInConversano = this.companyRepository.findByCitiesContaining(conversano);
 
-        assertThat(companiesInBari).hasSize(1);
         assertThat(companiesInConversano).hasSize(2);
+    }
+
+    @Test
+    public void test4LecceCompaniesTest() {
+        List<City> lecceArray = this.cityRepository.findByName("Lecce");
+        assertThat(lecceArray).hasSize(1);
+
+        City lecce = lecceArray.get(0);
+
+        List<Company> companiesInLecce = this.companyRepository.findByCitiesContaining(lecce);
+
+        assertThat(companiesInLecce).hasSize(1);
+    }
+
+    @Test
+    public void test5BrindisiCompaniesTest() {
+        List<City> brindisiArray = this.cityRepository.findByName("Brindisi");
+        assertThat(brindisiArray).hasSize(1);
+
+        City brindisi = brindisiArray.get(0);
+
+        List<Company> companiesInBrindisi = this.companyRepository.findByCitiesContaining(brindisi);
+
+        assertThat(companiesInBrindisi).hasSize(1);
+    }
+
+    @Test
+    public void test6RutiglianoCompaniesTest() {
+        List<City> rutiglianoArray = this.cityRepository.findByName("Rutigliano");
+        assertThat(rutiglianoArray).hasSize(1);
+
+        City rutigliano = rutiglianoArray.get(0);
+
+        List<Company> companiesInRutigliano = this.companyRepository.findByCitiesContaining(rutigliano);
+        assertThat(companiesInRutigliano).hasSize(0);
+    }
+
+    @Test
+    public void test7CassanoCompaniesTest() {
+        List<City> cassanoArray = this.cityRepository.findByName("Cassano delle Murge");
+        assertThat(cassanoArray).hasSize(1);
+
+        City cassano = cassanoArray.get(0);
+
+        List<Company> companiesInCassano = this.companyRepository.findByCitiesContaining(cassano);
+
+        assertThat(companiesInCassano).hasSize(1);
+    }
+
+    @Test
+    public void test8GioiaCompaniesTest() {
+        List<City> gioiaArray = this.cityRepository.findByName("Gioia del Colle");
+        assertThat(gioiaArray).hasSize(1);
+
+        City gioia = gioiaArray.get(0);
+
+        List<Company> companiesInGioia = this.companyRepository.findByCitiesContaining(gioia);
+
+        assertThat(companiesInGioia).hasSize(0);
     }
 }
